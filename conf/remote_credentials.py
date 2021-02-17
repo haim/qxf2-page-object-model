704fb7477e9815273f33354c41ed80ebfb6aa5e6
nilaya1234 <nilaya@qxf2.com>
2021-02-17 11:14:04 +0530

    changes

--- conf/remote_credentials.py
+++ conf/remote_credentials.py
@@ -2,5 +2,5 @@
 #SET REMOTE_BROWSER_PLATFORM TO SL TO RUN ON SAUCE LABS
 REMOTE_BROWSER_PLATFORM = "BS"
 
-USERNAME = "Enter sassas sasasasAXSsssasaxwzSsdaaddeqwezsssusjjjssaaeppppsssrname"
+USERNAME = "Enter sassas sasasasAXSssasssasaxwzSsdaaddeqwezsssusjjjssaaeppppsssrname"
 ACCESS_KEY = "Enter sasas access key"
